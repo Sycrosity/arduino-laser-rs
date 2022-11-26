d18507488e67733101eab8e4e384c44a83cd548a
Sycrosity <72102935+Sycrosity@users.noreply.github.com>
2022-11-26 17:41:32 +0000

    lcd control, beginning of servo control, deps

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,60 @@
 
 use panic_halt as _;
 
+use ag_lcd::{Blink, Cursor, Display, LcdDisplay};
+use arduino_hal::prelude::*;
+// use robust_arduino_serial::*;
+
+mod servo;
+
+use crate::servo::ServoUnit;
+
+use arduino_hal::simple_pwm;
+
 #[arduino_hal::entry]
 fn main() -> ! {
-    let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
+    let peripherals = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(peripherals);
+    let mut serial = arduino_hal::default_serial!(peripherals, pins, 57600);
+    let delay = arduino_hal::Delay::new();
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let rs = pins.d12.into_output().downgrade();
+    let en = pins.d11.into_output().downgrade();
+    let d4 = pins.d5.into_output().downgrade();
+    let d5 = pins.d4.into_output().downgrade();
+    let d6 = pins.d3.into_output().downgrade();
+    let d7 = pins.d2.into_output().downgrade();
 
-    let mut led = pins.d13.into_output();
+    let mut lcd: LcdDisplay<_, _> = LcdDisplay::new(rs, en, delay)
+        .with_half_bus(d4, d5, d6, d7)
+        .with_display(Display::On)
+        .with_blink(Blink::On)
+        .with_cursor(Cursor::On)
+        .build();
+
+    lcd.set_cursor(Cursor::Off);
+    lcd.set_blink(Blink::Off);
+
+    let text = "Hello World!";
+    lcd.print(text);
+
+    ufmt::uwriteln!(&mut serial, "Current text is: {}\r", text).void_unwrap();
+
+
+    let servo = pins.d8.into_output().downgrade();
+    
+
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        // Read a byte from the serial connection
+        let b = nb::block!(serial.read()).void_unwrap();
+
+        match b {
+            10u8 => (),
+            47u8 => lcd.clear(),
+            _ => lcd.write(b),
+        }
+
+        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
     }
 }
